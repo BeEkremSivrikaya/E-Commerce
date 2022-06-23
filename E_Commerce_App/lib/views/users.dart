8e0585d8dee44d6ec75bd1addb0cfde166e963bd
sivrikaya <88982607+BeEkremSivrikaya@users.noreply.github.com>
2022-06-23 17:29:48 +0300

    cards and users

--- E_Commerce_App/lib/views/users.dart
+++ E_Commerce_App/lib/views/users.dart
@@ -1,3 +1,4 @@
+import 'package:e_commerce_app/components/users_card.dart';
 import 'package:flutter/material.dart';
 
 class Users extends StatefulWidget {
@@ -9,9 +10,25 @@ class _UsersState extends State<Users> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-      title: Text("Users"),
-      centerTitle: true,
-    ));
+      appBar: AppBar(
+        title: Text("Users"),
+        centerTitle: true,
+      ),
+
+      //  body: Padding(
+      //     padding: const EdgeInsets.all(8.0),
+      //     child: Container(
+      //       padding: EdgeInsets.only(top: 16),
+      //       child: ListView.builder(
+      //         itemCount: Users.length,
+      //         shrinkWrap: true,
+      //         physics: ClampingScrollPhysics(),
+      //         itemBuilder: (context, index) {
+      //           return UsersCard(product: products[index]);
+      //         },
+      //       ),
+      //     ),
+      //   )
+    );
   }
 }
